5c36ec129d8ecf80162de285795fe14e2feef170
CaroNew <125692041+CaroNew@users.noreply.github.com>
2024-02-11 00:28:29 -0300

    agregada función para desencriptar

--- app.js
+++ app.js
@@ -7,23 +7,33 @@ function encrypt(msg, keys){
     let encryptedMsg = ""
   
     for(let i = 0; i < strSplit.length; i++) {
-    let key = strSplit[i];
-        if(keys[key]){
-            encryptedMsg += keys[key] 
-        }else{
-            encryptedMsg += key;
-        }
-     }
+        let key = strSplit[i];
+            if(keys[key]){
+                encryptedMsg += keys[key] 
+            }else{
+                encryptedMsg += key;
+            }
+    }
     return encryptedMsg;
 }
 
-function decrypt(msg){
-    return;
+function decrypt(msg, keys){
+    let strSplit = msg.split(" ");
+    let decryptedMsg = ""
+    console.log(strSplit.length);
+    for(let i = 0; i < strSplit.length; i++) {
+        let str = strSplit[i];
+        for(let key in keys){
+            if(str.indexOf(keys[key]) > -1){
+                str = str.replaceAll(keys[key], key); 
+            }
+        }
+        decryptedMsg += str + " ";  
+    }
+    return decryptedMsg;
 }
 
-function convertText(message, fun){
-    let encryptedMessage = "";
-  
+function convertText(message, fun){  
     const keys = {
       a: 'ai', 
       e: 'enter',
@@ -33,22 +43,33 @@ function convertText(message, fun){
     }
 
     if(fun === 'encrypt'){
-        encryptedMessage = encrypt(message, keys);
+        return encrypt(message, keys);
     }
-    
-
-    return encryptedMessage;
+    if(fun === 'decrypt'){
+        return decrypt(message, keys);
+    }
   }
 
 
 
-function getValue(input){
+function getValueEncrypt(input){
     let encryptedmsg = convertText(input, 'encrypt');
     console.log(encryptedmsg);
 }
 
-//evento del boton
+function getValuedecrypt(input){
+    let encryptedmsg = convertText(input, 'decrypt');
+    console.log(encryptedmsg);
+}
+
+//evento del boton encriptar
 btn.addEventListener("click",  () => {
-    getValue(textInput.value);
+    getValueEncrypt(textInput.value);
+    textInput.value = '';
+    });
+
+//evento del boton desencritar
+btn2.addEventListener("click",  () => {
+    getValuedecrypt(textInput.value);
     textInput.value = '';
     });
